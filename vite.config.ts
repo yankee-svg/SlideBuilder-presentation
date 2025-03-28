92409958d27fdd4b628ea3be0bb74d7e80bfec80
Emery... <e.barame@alustudent.com>
2025-03-28 08:06:58 +0200

    fixing bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+//using vite conf
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "slidebuilder-tagger";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
